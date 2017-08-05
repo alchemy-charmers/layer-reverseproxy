ee86640d021a08411b8b4c132d0aa54ae2e6de06
Chris Sanders <sanders.chris@gmail.com>
2017-08-04 23:00:50 -0400

    Added Readme

--- README.ex
+++ README.ex
@@ -1,65 +1,31 @@
 # Overview
 
-Describe the intended usage of this charm and anything unique about how this
-charm relates to others here.
-
-This README will be displayed in the Charm Store, it should be either Markdown
-or RST. Ideal READMEs include instructions on how to use the charm, expected
-usage, and charm features that your audience might be interested in. For an
-example of a well written README check out Hadoop:
-http://jujucharms.com/charms/precise/hadoop
-
-Use this as a Markdown reference if you need help with the formatting of this
-README: http://askubuntu.com/editing-help
-
-This charm provides [service][]. Add a description here of what the service
-itself actually does.
-
-Also remember to check the [icon guidelines][] so that your charm looks good
-in the Juju GUI.
+This charm implements the requires side of the [reverseproxy interface][reverseproxy]. This
+allows you to request and configure a reverseproxy on a machine that doesn't
+implemnt the interface.
 
 # Usage
 
-Step by step instructions on using the charm:
+To deploy:
 
-juju deploy servicename
+    juju deploy cs:~chris.sanders/reverseproxy --to 1
+    juju add-relation reverseproxy haproxy
 
-and so on. If you're providing a web service or something that the end user
-needs to go to, tell them here, especially if you're deploying a service that
-might listen to a non-default port.
-
-You can then browse to http://ip-address to configure the service.
-
-## Scale out Usage
-
-If the charm has any recommendations for running at scale, outline them in
-examples here. For example if you have a memcached relation that improves
-performance, mention it here.
-
-## Known Limitations and Issues
-
-This not only helps users but gives people a place to start if they want to help
-you add features to your charm.
+In the above example this charm is deployed to machine 1 and a relation is 
+added to the [haproxy charm][haproxy].
 
 # Configuration
 
-The configuration options will be listed on the charm store, however If you're
-making assumptions or opinionated decisions in the charm (like setting a default
-administrator password), you should detail that here so the user knows how to
-change it immediately, etc.
+See configuration options below.
 
 # Contact Information
 
-Though this will be listed in the charm store itself don't assume a user will
-know that, so include that information here:
-
 ## Upstream Project Name
 
-  - Upstream website
-  - Upstream bug tracker
-  - Upstream mailing list or contact information
-  - Feel free to add things if it's useful for users
+  - Code: https://github.com/chris-sanders/layer-reverseproxy
+  - Bug tracking: https://github.com/chris-sanders/layer-reverseproxy/issues
+  - Contact information: sanders.chris@gmail.com
 
 
-[service]: http://example.com
-[icon guidelines]: https://jujucharms.com/docs/stable/authors-charm-icon
+[haproxy]: https://jujucharms.com/u/chris.sanders/haproxy
+[reverseproxy]: https://github.com/chris-sanders/interface-reverseproxy
