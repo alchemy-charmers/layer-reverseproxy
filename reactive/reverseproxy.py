086228060114c5d50f0a4cf2dc59b98a16563454
Chris Sanders <sanders.chris@gmail.com>
2017-09-09 16:48:25 -0500

    Fix pep8

--- reactive/reverseproxy.py
+++ reactive/reverseproxy.py
@@ -3,30 +3,27 @@ from charmhelpers.core import hookenv
 
 import socket
 
+
 @when_not('reverseproxy-charm.installed')
 @when('layer-hostname.installed')
 def install_reverseproxy():
-    hookenv.status_set('maintenance','ready to configure')
+    hookenv.status_set('maintenance', 'ready to configure')
     set_state('reverseproxy-charm.installed')
 
-@when_all('reverseproxy.triggered','reverseproxy.ready','reverseproxy-charm.installed')
-@when_not('reverseproxy.configured','reverseproxy.departed')
-def configure_reverseproxy(reverseproxy,*args):
-    # TODO: retrigger if config change?
+
+@when_all('reverseproxy.triggered', 'reverseproxy.ready', 'reverseproxy-charm.installed')
+@when_not('reverseproxy.configured', 'reverseproxy.departed')
+def configure_reverseproxy(reverseproxy, *args):
     charm_config = hookenv.config()
     hookenv.log("Setting up reverseproxy", "INFO")
-    proxy_info = {'mode':charm_config['mode'],
-                  'urlbase':charm_config['urlbase'],
-                  'subdomain':charm_config['subdomain'],
-                  'group_id':charm_config['group-id'],
+    proxy_info = {'mode': charm_config['mode'],
+                  'urlbase': charm_config['urlbase'],
+                  'subdomain': charm_config['subdomain'],
+                  'group_id': charm_config['group-id'],
                   'external_port': charm_config['external-port'],
                   'internal_host': socket.getfqdn(),
                   'internal_port': charm_config['internal-port']
                   } 
     reverseproxy.configure(proxy_info)
-    hookenv.status_set('active','')
-
-#@when_all('reverseproxy.triggered','reverseproxy.departed')
-#def remove_urlbase(reverseproxy,*args):
-#    hookenv.log("Removing reverseproxy configuration", "INFO")
+    hookenv.status_set('active', '')
 
